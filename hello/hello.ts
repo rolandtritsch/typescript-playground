2ec1bc0fffe66050608ceebef0d797fb5212b73e
Roland Tritsch <roland@tritsch.email>
2024-11-17 12:33:41 +0000

    hello: Polish. v2.

--- hello/hello.ts
+++ hello/hello.ts
@@ -1,4 +1,4 @@
-let message: string = 'Hello, World!';
-let heading = document.createElement('h1');
+const message: string = 'Hello, World!';
+const heading = document.createElement('h1');
 heading.textContent = message;
 document.body.appendChild(heading);
